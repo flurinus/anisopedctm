adcc4b3c6990a28e3795666dccb79c2417d15171
flurinus <flurinus@gmail.com>
2015-10-12 10:27:25 +0200

    cleaned up, activated calibration

--- src/AnisoPedCTM.java
+++ src/AnisoPedCTM.java
@@ -18,56 +18,56 @@ public class AnisoPedCTM {
 		 */
 		ArrayList<String> expList = new ArrayList<String>();
 		
-//		expList.add("examples/scenarios/HKU/HKU78-drake_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU78-sbfd_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU78-weidmann_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU79-drake_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU79-sbfd_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU79-weidmann_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU80-drake_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU80-sbfd_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU80-weidmann_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU81-drake_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU81-sbfd_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU81-weidmann_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU82-drake_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU82-sbfd_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU82-weidmann_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU83-drake_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU83-sbfd_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU83-weidmann_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU84-drake_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU84-sbfd_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU84-weidmann_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU84-zero_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU85-drake_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU85-sbfd_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU85-weidmann_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU85-zero_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU86-drake_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU86-sbfd_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU86-weidmann_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU86-zero_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU87-drake_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU87-sbfd_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU87-weidmann_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU88-drake_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU88-sbfd_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU88-weidmann_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU88-zero_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU89-drake_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU89-sbfd_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU89-weidmann_scenario.txt");
-//		expList.add("examples/scenarios/HKU/HKU89-zero_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU78-drake_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU78-sbfd_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU78-weidmann_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU79-drake_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU79-sbfd_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU79-weidmann_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU80-drake_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU80-sbfd_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU80-weidmann_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU81-drake_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU81-sbfd_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU81-weidmann_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU82-drake_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU82-sbfd_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU82-weidmann_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU83-drake_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU83-sbfd_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU83-weidmann_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU84-drake_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU84-sbfd_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU84-weidmann_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU84-zero_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU85-drake_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU85-sbfd_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU85-weidmann_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU85-zero_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU86-drake_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU86-sbfd_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU86-weidmann_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU86-zero_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU87-drake_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU87-sbfd_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU87-weidmann_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU88-drake_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU88-sbfd_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU88-weidmann_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU88-zero_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU89-drake_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU89-sbfd_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU89-weidmann_scenario.txt");
+		expList.add("examples/scenarios/HKU/HKU89-zero_scenario.txt");
 		
-//		expList.add("examples/scenarios/BER-drake_scenario.txt");
-//		expList.add("examples/scenarios/BER-sbfd_scenario.txt");
-//		expList.add("examples/scenarios/BER-weidmann_scenario.txt");
-//		expList.add("examples/scenarios/BER-zero_scenario.txt");
+		expList.add("examples/scenarios/BER-drake_scenario.txt");
+		expList.add("examples/scenarios/BER-sbfd_scenario.txt");
+		expList.add("examples/scenarios/BER-weidmann_scenario.txt");
+		expList.add("examples/scenarios/BER-zero_scenario.txt");
 		
 		expList.add("examples/scenarios/BER-sbfd_scenario-visualized-notext.txt");
+		//demonstrates visualization
 		
-				
 		expList.parallelStream().forEach((exp) -> {
 			
 			//initialize simulation by generating board
@@ -85,25 +85,27 @@ public class AnisoPedCTM {
 		
 		
 		/*
-		 * Calibration (Berlin Experiments)
+		 * Calibration
 		 */
 		
 		int numIter = 256;
+		//WARNING: May take several hours to complete.
 		
 		ArrayList<String> calibList = new ArrayList<String>();
 		
-//		calibList.add("examples/scenarios/BER-drake_scenario.txt");
-//		calibList.add("examples/scenarios/BER-sbfd_scenario.txt");
-//		calibList.add("examples/scenarios/BER-weidmann_scenario.txt");
-//		calibList.add("examples/scenarios/BER-zero_scenario.txt");
+		calibList.add("examples/scenarios/BER-drake_scenario.txt");
+		calibList.add("examples/scenarios/BER-sbfd_scenario.txt");
+		calibList.add("examples/scenarios/BER-weidmann_scenario.txt");
+		calibList.add("examples/scenarios/BER-zero_scenario.txt");
 		
-//		calibList.add("examples/scenarios/HKU-drake_85_87.txt");
-//		calibList.add("examples/scenarios/HKU-sbfd_85_87.txt");
-//		calibList.add("examples/scenarios/HKU-weidmann_85_87.txt");
-//		calibList.add("examples/scenarios/HKU-zero_85_87.txt");
+		calibList.add("examples/scenarios/HKU-drake_85_87.txt");
+		calibList.add("examples/scenarios/HKU-sbfd_85_87.txt");
+		calibList.add("examples/scenarios/HKU-weidmann_85_87.txt");
+		calibList.add("examples/scenarios/HKU-zero_85_87.txt");
 		
-		expList.parallelStream().forEach((exp) -> {
-		
+		calibList.parallelStream().forEach((exp) -> {
+			System.out.println("Calibration run started.");
+			
 			//initialize simulation by generating board
 			Board board = new Board(exp);
 			
